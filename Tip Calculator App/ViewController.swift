2c6ba121285bf82284708ad67ce46b24c3bb4a18
Manny <sancheze0450@gmail.com>
2021-01-14 01:46:30 -0500

    Initial Commit

---
+++ Tip Calculator App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Tip Calculator App
+//
+//  Created by Emmanuel Sanchez on 1/14/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
